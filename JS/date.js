95dae490f29cf70addd626e8c07cee70a87618df
ZeldaSorceress <royalgalaxies.xoxo@gmail.com>
2020-12-16 04:59:51 -0500

    Version 1.6
    1. Fixed the side navigation issue
    
    2. Updated the appearance of the start menu
    
    3. Added new content to the start menu

--- JS/date.js
+++ JS/date.js
@@ -1,2 +1,18 @@
 var d = new Date();
-document.getElementById("date").innerHTML = d;
+var hours;
+var minutes;
+var timeOfDay = "AM";
+    hours = d.getHours();
+    minutes = d.getMinutes();
+
+    if (hours >= 12){
+        timeOfDay = "PM";
+    }
+    else {
+        timeOfDay = "AM";
+    }
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    var date = "" + hours + ":" + minutes + " " + timeOfDay;
+    document.getElementById("date").innerHTML = date;
