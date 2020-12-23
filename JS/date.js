b48707aca564d8032f860c64a446e42ca8fd8a71
ZeldaSorceress <royalgalaxies.xoxo@gmail.com>
2020-12-23 00:18:45 -0500

    Version 1.8.1
    1. Updated date.js

--- JS/date.js
+++ JS/date.js
@@ -5,7 +5,14 @@ var timeOfDay = "AM";
     hours = d.getHours();
     minutes = d.getMinutes();
 
-    if (hours >= 12){
+    if (hours == 0){
+        hours = 12;
+        timeOfDay = "AM";
+    }
+    else if (hours >= 12){
+        if (hours != 12){
+            hours = hours - 12;
+        }
         timeOfDay = "PM";
     }
     else {
