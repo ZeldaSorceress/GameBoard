a0be5d3f55e242df098c8f3457092edc55ff1185
ZeldaSorceress <royalgalaxies.xoxo@gmail.com>
2020-12-23 00:58:28 -0500

    Version 1.8.2
    1. Fixed date.js issue
    
    2. Added 3 memorable songs to each category in  mp3.html
    
    3. Updated JS file for mp3.html

--- JS/music.js
+++ JS/music.js
@@ -39,3 +39,129 @@ function zelda() {
       $('#play').removeClass('glyphicon-pause')
   }
 }
+
+function skyloft() {
+    var audio = document.getElementById('sky');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function groose() {
+    var audio = document.getElementById('groose');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function fitheme() {
+    var audio = document.getElementById('fi');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function intro() {
+    var audio = document.getElementById('intro');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function dungeon() {
+    var audio = document.getElementById('dungeon');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function secret() {
+    var audio = document.getElementById('secret');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function healing() {
+    var audio = document.getElementById('healing');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function clocktown() {
+    var audio = document.getElementById('clock');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
+
+  function stormsong() {
+    var audio = document.getElementById('storm');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+  }
